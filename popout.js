c3453b4f9419186ea74f0f7443985767abcc2fd1
Shubham Kumar <115333492+shubhamkumaar@users.noreply.github.com>
2024-02-28 13:59:01 +0530

    merge conflict solved

--- popout.js
+++ popout.js
@@ -1,1 +1,1 @@
-window.open('windows/whiteboard.html', 'whiteboard', 'width=800, height=600');  
+window.open('windows/whiteboard.html', "whiteboard", "width=800, height=600 ,left=500px,top=100px");  
